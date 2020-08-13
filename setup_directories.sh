9986fbe5c9ab494165b02dff79a4678fd8db3eb6
Andre Oliva <oliva@cpt-centauri.am10.uni-tuebingen.de>
2020-08-13 20:05:18 +0200

    +setup dirs

---
+++ setup_directories.sh
@@ -0,0 +1,5 @@
+# This small shell script creates the necessary directories for the automatically-generated files
+# Main instructions in Readme.md
+mkdir -p bin/so
+mkdir -p data/zt/meta
+mkdir -p gensrc
